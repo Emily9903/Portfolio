0fb6dd8d0d952de8312b4552b5ba02271a9953fc
Emily9903 <emily_9903@hotmail.com>
2022-07-12 16:55:35 +0100

    image added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import profilePic from "./profilePic.jpg"
+import profilePic from "./profilepic.jpg"
 
 function App() {
   return (
     <div className="App">
     <h1>Emily Vickery</h1>
     <h2>Full Stack Junior Software Developer</h2> 
-    <img src={profilePic} alt="profile pic"/>
+    <img src={profilePic} alt="profile pic" className="profilePic"/>
     <div>
     <h3>About Me</h3>
     <p>Some blurb about me, tbc</p>
