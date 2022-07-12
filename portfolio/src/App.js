a5e118528e1078ba87c2be79764029d7d011816d
Emily9903 <emily_9903@hotmail.com>
2022-07-12 15:33:53 +0100

    test with image

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import profilePic from "./profilePic.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Emily Vickery</h1>
+    <h2>Full Stack Junior Software Developer</h2> 
+    <img src={profilePic} alt="profile pic"/>
+    <div>
+    <h3>About Me</h3>
+    <p>Some blurb about me, tbc</p>
+    </div>
     </div>
   );
 }
