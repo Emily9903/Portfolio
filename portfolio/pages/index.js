5899d01988ee562e131ecd4e0dcc1a7c1bcc677d
Emily9903 <emily_9903@hotmail.com>
2022-07-25 18:29:51 +0100

    next app created

---
+++ portfolio/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import profilePic from '../public/picture.jpg'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Emily Vickery</h1>
+      <h2>Full Stack Junior Software Developer</h2>
+      <image src={profilePic} alt="Profile Picture"/>
+    </div>
+  )
+}
