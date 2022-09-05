1c4cdc5ce3f41c67d94a3c6394b47cc942b9a186
Emily9903 <emily_9903@hotmail.com>
2022-09-05 16:20:10 +0100

    new picture added

--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import profilePic from '../public/picture.jpg'
+import profilePic from '../public/picture1.jpg'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Emily Vickery</h1>
+      <div>
       <h2>Full Stack Junior Software Developer</h2>
-      <img src={profilePic} alt="Profile Picture"/>
+      <Image src={profilePic} height="200vh" width="200vw" alt="Profile Picture"/>
+      </div>
     </div>
   )
 }
